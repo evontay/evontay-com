85a6c2e321a478002fa14d3c3383181d997f686d
Evon <fumblies@gmail.com>
2016-08-21 18:29:37 +0800

    added fly-in animation on scroll for projects

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ $(function () {
   $('#nav').scrollspy()
 })
 
+// For navbar fade-in on scroll
 $(window).scroll(function () {
   console.log('hello top-fade')
   $('#top').css('opacity', 1 - $(window).scrollTop() / 350)
@@ -25,5 +26,35 @@ $(window).scroll(function () {
     $('#nav').fadeIn()
   } else {
     $('#nav').fadeOut()
+  }
+})
+
+// For elements floating into place on scroll
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop()
+
+  if (wScroll > $('.education-item').offset().top - $(window).height()) {
+    var offset = Math.min(0, wScroll - $('.proj1').offset().top + $(window).height() - 300)
+    $('.proj1').css({'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.2) + 'px)'})
+  }
+
+  if (wScroll > $('.education-item').offset().top - $(window).height()) {
+    var offset = Math.min(0, wScroll - $('.proj2').offset().top + $(window).height() - 300)
+    $('.proj2').css({'transform': 'translate(' + Math.abs(offset) + 'px, ' + Math.abs(offset * 0.2) + 'px)'})
+  }
+
+  if (wScroll > $('.education-item').offset().top - $(window).height()) {
+    var offset = Math.min(0, wScroll - $('.proj3').offset().top + $(window).height() - 300)
+    $('.proj3').css({'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.2) + 'px)'})
+  }
+
+  if (wScroll > $('.education-item').offset().top - $(window).height()) {
+    var offset = Math.min(0, wScroll - $('.proj4').offset().top + $(window).height() - 300)
+    $('.proj4').css({'transform': 'translate(' + Math.abs(offset) + 'px, ' + Math.abs(offset * 0.2) + 'px)'})
+  }
+
+  if (wScroll > $('.education-item').offset().top - $(window).height()) {
+    var offset = Math.min(0, wScroll - $('.proj5').offset().top + $(window).height() - 300)
+    $('.proj5').css({'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.2) + 'px)'})
   }
 })
