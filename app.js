bbcf6077f86e737ad9ce5920fa1f17e601499adc
Evon <fumblies@gmail.com>
2016-08-22 13:23:55 +0800

    project items fade into view on scroll

--- app.js
+++ app.js
@@ -14,14 +14,12 @@ $(function () {
   $('#nav').scrollspy()
 })
 
-// For navbar fade-in on scroll
+// For top-info fade-out on scroll
 $(window).scroll(function () {
-  console.log('hello top-fade')
   $('#top').css('opacity', 1 - $(window).scrollTop() / 350)
 })
-
+// For navbar fade-in on scroll
 $(window).scroll(function () {
-  console.log('hello navbar')
   if ($(this).scrollTop() > 200) {
     $('#nav').fadeIn()
   } else {
@@ -36,25 +34,31 @@ $(window).scroll(function () {
   if (wScroll > $('.education-item').offset().top - $(window).height()) {
     var offset = Math.min(0, wScroll - $('.proj1').offset().top + $(window).height() - 600)
     $('.proj1').css({'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.1) + 'px)'})
+    console.log('opacity should be (wScroll / $(proj).offset().top): ' + (wScroll / $('.proj1').offset().top))
+    $('.proj1').css('opacity', wScroll / $('.proj1').offset().top)
   }
 
   if (wScroll > $('.education-item').offset().top - $(window).height()) {
     var offset = Math.min(0, wScroll - $('.proj2').offset().top + $(window).height() - 600)
     $('.proj2').css({'transform': 'translate(' + Math.abs(offset) + 'px, ' + Math.abs(offset * 0.1) + 'px)'})
+    $('.proj2').css('opacity', wScroll / $('.proj2').offset().top)
   }
 
   if (wScroll > $('.education-item').offset().top - $(window).height()) {
     var offset = Math.min(0, wScroll - $('.proj3').offset().top + $(window).height() - 600)
     $('.proj3').css({'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.1) + 'px)'})
+    $('.proj3').css('opacity', wScroll / $('.proj3').offset().top)
   }
 
   if (wScroll > $('.education-item').offset().top - $(window).height()) {
     var offset = Math.min(0, wScroll - $('.proj4').offset().top + $(window).height() - 600)
     $('.proj4').css({'transform': 'translate(' + Math.abs(offset) + 'px, ' + Math.abs(offset * 0.1) + 'px)'})
+    $('.proj4').css('opacity', wScroll / $('.proj4').offset().top)
   }
 
   if (wScroll > $('.education-item').offset().top - $(window).height()) {
     var offset = Math.min(0, wScroll - $('.proj5').offset().top + $(window).height() - 600)
     $('.proj5').css({'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.1) + 'px)'})
+    $('.proj5').css('opacity', wScroll / $('.proj5').offset().top)
   }
 })
